5c265bb1fc8d1bae6d621f6f518d8028418a5563
Nin-Shinobi <ninshinobi11@gmail.com>
2026-05-12 09:10:29 +0200

    update_con_dbb

--- Pfolio/connexion.inc.php
+++ Pfolio/connexion.inc.php
@@ -1,7 +1,7 @@
 <?php
 $host ='localhost';
-$user = 'nyx';
-$password ='Noxus@Freya_11';
+$user = '';
+$password ='';
 $bdd ='l1irtg4';
 
 $conn = mysqli_connect($host, $user, $password, $bdd) or die("Message d\'erreur : " . mysqli_connect_error());
