279c71f989076b68f557d3f0a0a375b516b51f01
𝕹𝖎𝖓_𝕾𝖍𝖎𝖓𝖔𝖇𝖎🥷🏾 <ninshinobi11@gmail.com>
2026-05-18 03:07:11 +0100

    Fix HTML structure in login.inc.php

--- Pfolio/login.inc.php
+++ Pfolio/login.inc.php
@@ -1,9 +1,10 @@
 <section id="Login" class="reveal">
-            <div class="dm">
-                <h2>Connectez Vous</h2>
-                <form id="login-form" method='post' action='index.php?pg='>
-                    <input type="text" placeholder="Login" required name='nom'>
-                    <input type="password" placeholder="password" required name='email'>
-                    <button class="but" type="submit">Connexion</button>
-                </form>
+    <div class="dm">
+        <h2>Connectez Vous</h2>
+        <form id="login-form" method="post" action="index.php?pg=">
+            <input type="text" placeholder="Login" required name="nom">
+            <input type="password" placeholder="password" required name="email">
+            <button class="but" type="submit">Connexion</button>
+        </form>
+    </div>
 </section>
